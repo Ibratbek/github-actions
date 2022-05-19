3f243d39879f9a8d1d94630816ffa3cb6b38df5d
Username <tillaberdiyevibrat@gmail.com>
2022-05-19 12:29:42 +0500

    initial

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+app.get('/users', (req, res) => {
+  res.send('All users');
+});
+
+app.use('*', (req, res) => {
+  res.send('Not found!');
+});
+
+const port = process.env.APP_PORT;
+
+app.listen(port, () => console.log(`server is run on port: ${port}`));
