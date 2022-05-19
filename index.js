f7978e4584d09ca024aa8bd1aadf262598f2cc76
Username <tillaberdiyevibrat@gmail.com>
2022-05-19 13:07:31 +0500

    feat: new api

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get('/users', (req, res) => {
   res.send('All users');
 });
 
+app.get('/docutents', (req, res) => {
+  res.send('All documents');
+});
+
 app.use('*', (req, res) => {
   res.send('Not found!');
 });
